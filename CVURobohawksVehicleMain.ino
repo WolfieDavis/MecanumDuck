f92fdaad2a9036e34cb5b7b0c591888189294300
Cailean Sorce <caileansorce@gmail.com>
2022-05-31 13:37:54 -0400

    added the working WiFi files

--- CVURobohawksVehicleMain.ino
+++ CVURobohawksVehicleMain.ino
@@ -8,7 +8,7 @@
 // #include <Servo.h>  //used to easily control the wheel servos
 #include <EEPROM.h> //used to store the servo errors in EEPROM (flash) memoory, so that it can be saved through power down
 // #include <SparkFun_TB6612.h>
-#include "CVURobohawksVehicleWifi.h" //contains the html + js and funcitons used to set up and interface with the
+#include "CVURobohawksVehicleWiFi.h" //contains the html + js and funcitons used to set up and interface with the
 // wifi client that will be used to control the vehicle
 // #include "CaileanSorce_nodemcuESP8266_pinDefinitions.h" //contains the const ints used to convert the GPIO port names silkscreened on the
 // dev board to the esp8266 GPIO numbers
